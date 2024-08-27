9001c075ec92aa8e8d17ce4f5558567ca5a1a2bf
a-m-n-a <amnazamir1234@gmail.com>
2024-08-27 21:30:56 +0500

    initial commit

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ app.post('/analyze-case', async (req, res) => {
     }
 });
 
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
